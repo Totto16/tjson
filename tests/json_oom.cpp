d728bfa42ff2b0552098e66e7857b7f76979807a
Totto16 <tobiausgais@gmail.com>
2026-04-22 20:36:30 +0200

    feat: add more oom tests

--- tests/json_oom.cpp
+++ tests/json_oom.cpp
@@ -181,6 +181,95 @@ TEST_CASE("testing oom behaviour of json functions <json_oom_tester>") {
 			REQUIRE_EQ(expected_error, actual_error);
 		}();
 	}
+
+	SUBCASE("parsing empty array fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.malloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "[]";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error = JsonErrorCpp::with_string_loc(
+			    "Internal OOM error: array allocation failed while parsing an empty array",
+			    dummy_str_view, JsonSourcePosition{ .line = 0, .col = 2 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
+
+	SUBCASE("parsing non-empty array fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.malloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "[1,2]";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error = JsonErrorCpp::with_string_loc(
+			    "Internal OOM error: array allocation failed while parsing an array",
+			    dummy_str_view, JsonSourcePosition{ .line = 0, .col = 1 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
+
+	SUBCASE("parsing: array entry add fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.realloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "[1]";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error = JsonErrorCpp::with_string_loc(
+			    "json array add error", dummy_str_view, JsonSourcePosition{ .line = 0, .col = 2 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
 }
 
 TEST_SUITE_END();
