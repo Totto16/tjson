765f23fd8fe7760f5cd1a40b5b7c68e0568dea2a
Totto16 <tobiausgais@gmail.com>
2026-04-22 20:03:41 +0200

    fix: fix cpp implementation of the global mock allocators - do some c++ RAII / lifetime / move tricks, so that it works as expected

--- tests/helpers/allocators.hpp
+++ tests/helpers/allocators.hpp
@@ -2,40 +2,44 @@
 
 #pragma once
 
-#include <functional>
-#include <memory>
+#include <cstddef>
 
 namespace mock {
 
 using FunctionMockMember = struct FunctionMockMemberImpl;
 
 class FunctionMock {
-	using Ptr = std::unique_ptr<FunctionMockMember, std::function<void(FunctionMockMember*)>>;
-
-	Ptr m_impl;
+	FunctionMockMember* m_impl;
 
   public:
 	FunctionMock();
 
-	FunctionMock(Ptr&& member);
+	explicit FunctionMock(FunctionMockMember*&& member);
 
 	[[nodiscard]] bool always_fail() const;
 	[[nodiscard]] bool fail_after(size_t count) const;
 	[[nodiscard]] bool never_fail() const;
+
+	FunctionMock(FunctionMock const&) = delete;
+	FunctionMock& operator=(FunctionMock const&) = delete;
+
+	FunctionMock(FunctionMock&&) noexcept;
+	FunctionMock& operator=(FunctionMock&&) noexcept;
+
+	~FunctionMock();
 };
 
 class CMockAllocator {
-	// this is a singleton!
-	static std::unique_ptr<CMockAllocator> s_instance;
 	// members
 	FunctionMock m_malloc;
 	FunctionMock m_calloc;
 	FunctionMock m_realloc;
+	void* m_associated_instance;
 
-	CMockAllocator();
+	explicit CMockAllocator(void* associated_instance);
 
   public:
-	[[nodiscard]] static const std::unique_ptr<CMockAllocator>& instantiate();
+	[[nodiscard]] static CMockAllocator get_instance();
 
 	[[nodiscard]] const FunctionMock& malloc() const;
 
@@ -46,7 +50,10 @@ class CMockAllocator {
 	CMockAllocator(CMockAllocator const&) = delete;
 	CMockAllocator& operator=(CMockAllocator const&) = delete;
 
-	~CMockAllocator();
+	CMockAllocator(CMockAllocator&&) noexcept;
+	CMockAllocator& operator=(CMockAllocator&&) noexcept;
+
+	~CMockAllocator() noexcept(false);
 };
 
 } // namespace mock
