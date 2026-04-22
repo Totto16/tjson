cde6bd0d8142e5082ea45b18f2bbd585acc478b7
Totto16 <tobiausgais@gmail.com>
2026-04-22 20:25:25 +0200

    feat: add more oom tests

--- tests/json_oom.cpp
+++ tests/json_oom.cpp
@@ -14,7 +14,7 @@ TEST_SUITE_BEGIN("json_oom" * doctest::description("json oom tests") * doctest::
 
 TEST_CASE("testing oom behaviour of json functions <json_oom_tester>") {
 
-	SUBCASE("first alloc fails") {
+	SUBCASE("parsing string fails") {
 		[]() -> void {
 			const auto mock_allocator = mock::CMockAllocator::get_instance();
 
@@ -37,7 +37,98 @@ TEST_CASE("testing oom behaviour of json functions <json_oom_tester>") {
 			// just here as a dummy tstr_view
 			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
 			auto expected_error = JsonErrorCpp::with_string_loc(
-			    "Internal OOM error", dummy_str_view, JsonSourcePosition{ .line = 0, .col = 1 });
+			    "Internal OOM error: string allocation failed while parsing one", dummy_str_view,
+			    JsonSourcePosition{ .line = 0, .col = 1 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
+
+	SUBCASE("parsing empty object fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.malloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "{}";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error = JsonErrorCpp::with_string_loc(
+			    "Internal OOM error: object allocation failed while parsing an empty object",
+			    dummy_str_view, JsonSourcePosition{ .line = 0, .col = 2 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
+
+	SUBCASE("parsing non-empty object fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.malloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "{\"hello\"}";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error = JsonErrorCpp::with_string_loc(
+			    "Internal OOM error: object allocation failed while parsing an object",
+			    dummy_str_view, JsonSourcePosition{ .line = 0, .col = 1 });
+
+			REQUIRE_EQ(actual_error, expected_error);
+		}();
+	}
+
+	SUBCASE("parsing: object entry add fails") {
+		[]() -> void {
+			const auto mock_allocator = mock::CMockAllocator::get_instance();
+
+			const bool mock_res = mock_allocator.calloc().always_fail();
+			REQUIRE_TRUE(mock_res);
+
+			std::string json_str = "{\"hello\":null}";
+
+			const tstr_view str_view = helpers::tstr_view_from_str(json_str);
+
+			const auto parse_result = json_value_parse_from_str(str_view);
+
+			REQUIRE_EQ(get_current_tag_type_for_json_parse_result(parse_result),
+			           JsonParseResultTypeError);
+
+			JsonError result = json_parse_result_get_as_error(parse_result);
+
+			const auto actual_error = JsonErrorCpp{ result };
+
+			// just here as a dummy tstr_view
+			const auto dummy_str_view = tstr_view_from("__dummy_str_view__impl__");
+			auto expected_error =
+			    JsonErrorCpp::with_string_loc("json object add error", dummy_str_view,
+			                                  JsonSourcePosition{ .line = 0, .col = 13 });
 
 			REQUIRE_EQ(actual_error, expected_error);
 		}();
