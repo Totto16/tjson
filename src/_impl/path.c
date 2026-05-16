41cb2ec570a313446ab14bca40e0af57364fdab1
Totto16 <tobiausgais@gmail.com>
2026-05-16 16:09:49 +0200

    fix: fix clang-format formatting

--- src/_impl/path.c
+++ src/_impl/path.c
@@ -78,8 +78,7 @@ NODISCARD ReadFileResult read_entire_file(const tstr* const file_path) {
 			if(read_result == 0) {
 				if(remaining_size != 0) {
 					FREE_AT_END();
-					return new_read_file_result_error(
-					    TSTR_STATIC_LIT("Read to few data"));
+					return new_read_file_result_error(TSTR_STATIC_LIT("Read to few data"));
 				}
 
 				break;
